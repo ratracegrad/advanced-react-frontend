5f5816ba31cb4c62f80a684d870021eb0247aa2e
Jennifer Bland <ratracegrad@gmail.com>
2021-02-07 17:23:14 -0500

    Added ability to create a new product. Added ability to delete an existing product

--- pages/sell.js
+++ pages/sell.js
@@ -1,7 +1,9 @@
-import { useState } from 'react';
 import CreateProduct from '../components/CreateProduct';
 
 export default function SellPage() {
-  const [name, setName] = useState('');
-  return <CreateProduct />;
+  return (
+    <div>
+      <CreateProduct />
+    </div>
+  );
 }
