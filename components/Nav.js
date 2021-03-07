e24b031aba7fa01da481bbe1531cc37bb84e31c9
Jennifer Bland <ratracegrad@gmail.com>
2021-03-07 11:43:58 -0500

    Added a signin page

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,27 @@
 import Link from 'next/link';
+import SignOut from './signout';
 import NavStyles from './styles/NavStyles';
+import { useUser } from './User';
 
-const Nav = () => (
-  <NavStyles>
-    <Link href="/products">Products</Link>
-    <Link href="/sell">Sell</Link>
-    <Link href="/orders">Orders</Link>
-    <Link href="/account">Account</Link>
-  </NavStyles>
-);
+export default function Nav() {
+  const user = useUser();
 
-export default Nav;
+  return (
+    <NavStyles>
+      <Link href="/products">Products</Link>
+      {user && (
+        <>
+          <Link href="/sell">Sell</Link>
+          <Link href="/orders">Orders</Link>
+          <Link href="/account">Account</Link>
+          <SignOut />
+        </>
+      )}
+      {!user && (
+        <>
+          <Link href="/signin">Sign In</Link>
+        </>
+      )}
+    </NavStyles>
+  );
+}
