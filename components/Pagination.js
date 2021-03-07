b343d8c4a5e3685f83d948b2c1712231538fbae4
Jennifer Bland <ratracegrad@gmail.com>
2021-03-07 11:45:56 -0500

    Added pagination to display of products page

--- components/Pagination.js
+++ components/Pagination.js
@@ -16,23 +16,25 @@ export const PAGINATION_QUERY = gql`
 
 export default function Pagination({ page }) {
   const { error, loading, data } = useQuery(PAGINATION_QUERY);
-  if (loading) return null;
+  if (loading) return 'Loading...';
   if (error) return <DisplayError error={error} />;
+  console.log('data', data);
   const { count } = data._allProductsMeta;
   const pageCount = Math.ceil(count / perPage);
-
   return (
     <PaginationStyles>
       <Head>
-        <title>Sick Fits - Page {page} of ____</title>
+        <title>
+          Sick Fits - Page {page} of {pageCount}
+        </title>
       </Head>
-      <Link href={`/products/${page - 1}`} aria-disabled={page === 1}>
+      <Link href={`/products/${page - 1}`}>
         <a aria-disabled={page <= 1}>← Prev</a>
       </Link>
       <p>
         Page {page} of {pageCount}
       </p>
-      <p>{count} Items total</p>
+      <p>{count} Items Total</p>
       <Link href={`/products/${page + 1}`}>
         <a aria-disabled={page >= pageCount}>Next →</a>
       </Link>
